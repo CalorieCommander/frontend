c8c18e8a8877427bf7df53bc97d48fd19af5767d
cerchio22 <cerchiogerrits10@gmail.com>
2024-03-28 15:56:30 +0100

    change
    kleur toepassing

--- app/src/App.js
+++ app/src/App.js
@@ -2,18 +2,22 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Inloggen from "./pages/Inloggen";
 import Registreren from "./pages/Registreren";
+import logo from "./img/caloriecommander.png";
 
 function App() {
   return (
     <Router>
       <div className="Test">
-        <nav>
-          <ul>
-            <Link to="/pages/Inloggen">Inloggen</Link>
-            <br />
-            <Link to="/pages/Registreren">Registreren</Link>
-          </ul>
-        </nav>
+        <div className="navbar">
+          <img src={logo} className="logo" />
+        </div>
+        <div className="line"></div>
+        <ul>
+          <Link to="/pages/Inloggen">Inloggen</Link>
+          <br />
+          <Link to="/pages/Registreren">Registreren</Link>
+        </ul>
+
         <Routes>
           <Route path="/pages/Inloggen" element={<Inloggen />} />
           <Route path="/pages/Registreren" element={<Registreren />} />
