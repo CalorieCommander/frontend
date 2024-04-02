d998d599570679eb38d047b047efa021947bc9cd
cerchio22 <cerchiogerrits10@gmail.com>
2024-04-02 13:53:25 +0200

    startpagina
    startpagina aanpassen

--- app/src/App.js
+++ app/src/App.js
@@ -5,26 +5,49 @@ import Registreren from "./pages/Registreren";
 import logo from "./img/caloriecommander.png";
 import ananas from "./img/fruit.png";
 import kiwi from "./img/kiwi.png";
+import watermelon from "./img/watermelon.png";
+import peach from "./img/peach.png";
+import banana from "./img/banana.png";
 
 function App() {
   return (
     <Router>
       <div className="Test">
         <div className="navbar">
-          <img src={logo} className="logo" />
+          <img src={logo} alt="logo" className="logo" />
         </div>
         <div className="line"></div>
-        <img src={ananas} className="fruit-ananas" />
-        <img src={kiwi} className="fruit-kiwi" />
-        <ul>
-          <Link to="/pages/Inloggen" className="inlogpage">
-            Inloggen
-          </Link>
+        <img src={ananas} alt="ananas" className="fruit-ananas" />
+        <img src={kiwi} alt="kiwi" className="fruit-kiwi" />
+        <img src={watermelon} alt="watermelon" className="fruit-watermelon" />
+        <img src={peach} alt="peach" className="fruit-peach" />
+        <img src={banana} alt="banana" className="fruit-banana" />
+
+        <img src={ananas} alt="ananas" className="fruit-ananas right" />
+        <img src={kiwi} alt="kiwi" className="fruit-kiwi right" />
+        <img
+          src={watermelon}
+          alt="watermelon"
+          className="fruit-watermelon right"
+        />
+        <img src={peach} alt="peach" className="fruit-peach right" />
+        <img src={banana} alt="banana" className="fruit-banana right" />
+        <div className="tekst">
+          WELKOM BIJ
           <br />
-          <Link to="/pages/Registreren" className="registratiepage">
-            Registreren
-          </Link>
-        </ul>
+          CALORIECOMMANDER
+        </div>
+        <div className="blauw-tekst">
+          Meld je hieronder aan om je leven
+          <br />
+          voorgoed te veranderen!
+        </div>
+        <Link to="/pages/Inloggen" className="button">
+          Inloggen
+        </Link>
+        <Link to="/pages/Registreren" className="button2">
+          Registreren
+        </Link>
 
         <Routes>
           <Route path="/pages/Inloggen" element={<Inloggen />} />
