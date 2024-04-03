9decc97c455065bfcbb118ab37215590a1588b09
cerchio22 <cerchiogerrits10@gmail.com>
2024-04-03 11:35:11 +0200

    update
    update

--- app/src/App.js
+++ app/src/App.js
@@ -24,13 +24,19 @@ function App() {
         <img src={watermelon} alt="watermelon" className="fruit-watermelon" />
         <img src={peach} alt="peach" className="fruit-peach" />
         <img src={banana} alt="banana" className="fruit-banana" />
+        <img src={pear} alt="pear" className="fruit-pear" />
+        <img src={apple} alt="apple" className="fruit-apple" />
 
-        <img src={pear} alt="pear" className="fruit-ananas right" />
+        <img src={pear} alt="pear" className="fruit-pear right" />
         <img src={banana} alt="banana" className="fruit-kiwi right" />
         <img src={apple} alt="apple" className="fruit-watermelon right" />
         <img src={peach} alt="peach" className="fruit-peach right" />
         <img src={kiwi} alt="kiwi" className="fruit-banana right" />
+<<<<<<< Updated upstream
 
+=======
+        <img src={watermelon} alt="watermelon" className="fruit-apple right" />
+>>>>>>> Stashed changes
         <Routes>
           <Route path="/pages/Inloggen" element={<Inloggen />} />
           <Route path="/pages/Registreren" element={<Registreren />} />
