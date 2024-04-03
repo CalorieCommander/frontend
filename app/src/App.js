825e8de9538049f1565ca2b551a4d5fd67cc8873
cerchio22 <cerchiogerrits10@gmail.com>
2024-04-03 09:51:25 +0200

    change
    hoi

--- app/src/App.js
+++ app/src/App.js
@@ -17,31 +17,7 @@ function App() {
           <img src={logo} alt="logo" className="logo" />
         </div>
         <div className="line"></div>
-        <img src={ananas} alt="ananas" className="fruit-ananas" />
-        <img src={kiwi} alt="kiwi" className="fruit-kiwi" />
-        <img src={watermelon} alt="watermelon" className="fruit-watermelon" />
-        <img src={peach} alt="peach" className="fruit-peach" />
-        <img src={banana} alt="banana" className="fruit-banana" />
 
-        <img src={ananas} alt="ananas" className="fruit-ananas right" />
-        <img src={kiwi} alt="kiwi" className="fruit-kiwi right" />
-        <img
-          src={watermelon}
-          alt="watermelon"
-          className="fruit-watermelon right"
-        />
-        <img src={peach} alt="peach" className="fruit-peach right" />
-        <img src={banana} alt="banana" className="fruit-banana right" />
-        <div className="tekst">
-          WELKOM BIJ
-          <br />
-          CALORIECOMMANDER
-        </div>
-        <div className="blauw-tekst">
-          Meld je hieronder aan om je leven
-          <br />
-          voorgoed te veranderen!
-        </div>
         <Link to="/pages/Inloggen" className="button">
           Inloggen
         </Link>
