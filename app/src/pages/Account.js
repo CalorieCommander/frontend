1e4e181b4c1508fdf45014aba794759e39c222f5
cerchio22 <cerchiogerrits10@gmail.com>
2024-04-08 13:11:58 +0200

    xs
    xs

---
+++ app/src/pages/Account.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Account = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>We are a team of passionate developers building awesome React apps!</p>
+    </div>
+  );
+};
+
+export default Account;
