730e2f551d1b252647dd27a926bc56c008694e3f
cerchio22 <cerchiogerrits10@gmail.com>
2024-04-08 13:31:26 +0200

    ds
    laatste update

--- app/src/pages/Account.js
+++ app/src/pages/Account.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Account = () => {
+const AccountPage = () => {
   return (
     <div>
-      <h1>About Us</h1>
-      <p>We are a team of passionate developers building awesome React apps!</p>
+      <h1>Welcome to My Website!</h1>
+      <p>This is the home page of my React app.</p>
     </div>
   );
 };
 
-export default Account;
+export default AccountPage;
