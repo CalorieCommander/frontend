6632a1e5038e50f73ae2fd62acf6c9c5c0af684a
cerchio22 <90753573+cerchio22@users.noreply.github.com>
2024-04-10 11:28:02 +0200

    Update App.js
    update

--- app/src/App.js
+++ app/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <BrowserRouter>
         <Routes>
-        <Route path="" element={<Homepage />} />
+          <Route path="" element={<Homepage />} />
           <Route path="/Inloggen" element={<Inloggen />} />
           <Route path="/Registreren" element={<Registreren />} />
         </Routes>
