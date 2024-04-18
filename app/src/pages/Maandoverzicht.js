7b8d918ab0ba7acbd93b31b9bde8040d5e60612c
diede110 <diedevandijk8@gmail.com>
2024-04-18 10:09:33 +0200

    maandoverzicht

--- app/src/pages/Maandoverzicht.js
+++ app/src/pages/Maandoverzicht.js
@@ -105,14 +105,7 @@ const Maandoverzicht = () => {
       <div class="wrapper">
         <header>
           <p class="current-date"></p>
-          <div class="icons">
-            <span id="prev" class="material-symbols-rounded">
-              chevron_left
-            </span>
-            <span id="next" class="material-symbols-rounded">
-              chevron_right
-            </span>
-          </div>
+   
         </header>
         <div class="calendar">
           <ul class="weeks">
