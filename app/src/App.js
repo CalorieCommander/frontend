24ba8dedc093681c470c3d7607c3b6394e708b8d
cerchio22 <cerchiogerrits10@gmail.com>
2024-04-18 12:30:19 +0200

    update
    naam fixen
    op 3 paginas

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
-import { BrowserRouter as Switch, Route, Routes } from "react-router-dom";
-import './index.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+import "./index.css";
 // @ts-ignore
 import Homepage from "./pages/Homepage";
 import Accountpagina from "./pages/Accountpagina";
@@ -10,27 +15,42 @@ import Dagoverzicht from "./pages/Dagoverzicht";
 import Maandoverzicht from "./pages/Maandoverzicht";
 import logo from "./img/caloriecommander.png";
 
-
-function App() {
+const App = () => {
   return (
-    <Switch>
-        <div className="navbar">
-          <img src={logo} alt="logo" className="logo" />
-          <div className="name-corner">CERCHIO</div>
-        </div>
-        <div className="line"></div>
-        <Routes>
-          <Route path="/pages/Account" element={<Accountpagina />} />
-        </Routes>
-        <Routes>
-          <Route exact path="" element={<Homepage />} />
-          <Route exact path="/login" element={<Inloggen />} />
-          <Route exact path="/account" element={<Accountpagina />} />
-          <Route exact path="/register" element={<Registreren />} />
-          <Route exact path="/dagoverzicht" element={<Dagoverzicht />} />
-          <Route exact path="/maandoverzicht" element={<Maandoverzicht />} />
-        </Routes>
-    </Switch>
+    <Router>
+      <AppContent />
+    </Router>
   );
-}
+};
+
+const AppContent = () => {
+  const location = useLocation();
+  const showName =
+    location.pathname !== "/" &&
+    location.pathname !== "/login" &&
+    location.pathname !== "/register" &&
+    location.pathname !== "/maandoverzicht"; // Voeg hier andere pagina's toe waarop de naam niet moet worden weergegeven
+
+  return (
+    <div>
+      <div className="navbar">
+        <img src={logo} alt="logo" className="logo" />
+        {showName && <div className="name-corner">CERCHIO</div>}
+      </div>
+      <div className="line"></div>
+      <Routes>
+        <Route path="/pages/Account" element={<Accountpagina />} />
+      </Routes>
+      <Routes>
+        <Route exact path="/" element={<Homepage />} />
+        <Route exact path="/login" element={<Inloggen />} />
+        <Route exact path="/account" element={<Accountpagina />} />
+        <Route exact path="/register" element={<Registreren />} />
+        <Route exact path="/dagoverzicht" element={<Dagoverzicht />} />
+        <Route exact path="/maandoverzicht" element={<Maandoverzicht />} />
+      </Routes>
+    </div>
+  );
+};
+
 export default App;
