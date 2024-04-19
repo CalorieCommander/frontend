0a4f694daef8fea09b76019f2927ab19a8c7414d
cerchio22 <cerchiogerrits10@gmail.com>
2024-04-19 14:17:19 +0200

    update van naam in corner
    naam in de corner verwijderd op de homepage, inlogpage en registratiepage

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
-import { BrowserRouter as Switch, Route, Routes, Link } from "react-router-dom";
-import './index.css';
-// @ts-ignore
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+import "./index.css";
 import Homepage from "./pages/Homepage";
 import Accountpagina from "./pages/Accountpagina";
 import Inloggen from "./pages/Inloggen";
@@ -10,30 +14,41 @@ import Dagoverzicht from "./pages/Dagoverzicht";
 import Maandoverzicht from "./pages/Maandoverzicht";
 import logo from "./img/caloriecommander.png";
 
-
-function App() {
-  const clickeddate = new Date();
+const App = () => {
   return (
-    <Switch>
+    <Router>
+      <AppContent />
+    </Router>
+  );
+};
+
+const AppContent = () => {
+  const location = useLocation();
+  const showName =
+    location.pathname !== "/" &&
+    location.pathname !== "/login" &&
+    location.pathname !== "/register";
+
+  return (
+    <div>
       <div className="navbar">
-        <Link to="/maandoverzicht">
-          <img src={logo} alt="logo" className="logo" />
-        </Link>
-        <div className="name-corner">CERCHIO</div>
+        <img src={logo} alt="logo" className="logo" />
+        {showName && <div className="name-corner">CERCHIO</div>}
       </div>
       <div className="line"></div>
       <Routes>
         <Route path="/pages/Account" element={<Accountpagina />} />
       </Routes>
       <Routes>
-        <Route exact path="" element={<Homepage />} />
+        <Route exact path="/" element={<Homepage />} />
         <Route exact path="/login" element={<Inloggen />} />
         <Route exact path="/account" element={<Accountpagina />} />
         <Route exact path="/register" element={<Registreren />} />
         <Route exact path="/dagoverzicht" element={<Dagoverzicht />} />
         <Route exact path="/maandoverzicht" element={<Maandoverzicht />} />
       </Routes>
-    </Switch>
+    </div>
   );
-}
+};
+
 export default App;
